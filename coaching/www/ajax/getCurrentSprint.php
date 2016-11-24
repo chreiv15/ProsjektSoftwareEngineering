8570efc1fc733efb46d89426e1d6f074416e1c60
Fredrik Juel Hagen <fredrik.juel.hagen@gmail.com>
2016-11-24 11:42:25 +0100

    Fikset bugs og lagt til progress bar.

--- coaching/www/ajax/getCurrentSprint.php
+++ coaching/www/ajax/getCurrentSprint.php
@@ -9,6 +9,6 @@ echo '<hr>'."\n\r";
 echo '<p class="current-text">Gjenstående beløp <span class="text-success"><br>'.$sprint['spending'].'/'.$sprint['currentSprintGoal'].'</span></p>'."\n\r";
 echo '<i class="fa fa-cutlery category-icon" aria-hidden="true"></i>';
 echo '<p class="current-days">'.$sprint['date']['diff'].' dager gjenstår</p>';
-echo '<div class="progress-days"></div>';
+echo '<div class="progress-days"><progress max="30" value="'.(30-$sprint['date']['diff']).'"></progress></div>';
 
 ?>
