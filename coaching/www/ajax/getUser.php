861ceed75acadb189c63ee31f1c49aaab7e81a77
Fredrik Juel Hagen <fredrik.juel.hagen@gmail.com>
2016-11-24 12:41:40 +0100

    Automatisk login på standardbruker

--- coaching/www/ajax/getUser.php
+++ coaching/www/ajax/getUser.php
@@ -1,10 +1,10 @@
 <?php
 
-require_once '../functions.php';
+require_once '../../../functions.php';
 
 $email = val($_GET['user']);
 $password = val($_GET['password']);
 
-echo getUser($email, $password);
+getUser($email, $password);
 
 ?>
