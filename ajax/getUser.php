dfe8da80ebb88ffd87f99763f89b0dc82f21bef7
Fredrik Juel Hagen <fredrik.juel.hagen@gmail.com>
2017-03-02 10:49:11 +0100

    Slettet malplasserte endepunkter. Lagt til redirekt etter PIN.

--- ajax/getUser.php
+++ ajax/getUser.php
@@ -16,8 +16,13 @@ if(mysqli_num_rows($response)>0){
     $firstname = $row['firstname'];
     $lastname = $row['lastname'];
     $email = $row['email'];
-    $content = "$id|$firstname|$lastname|$email";
-    setcookie("login", $content, time()+3600, "/");
+    $json = array();
+    array_push($json, 'id' => $id);
+    array_push($json, 'firstname' => $firstname);
+    array_push($json, 'lastname' => $lastname);
+    array_push($json, 'email' => $email);
+    $json = json_encode($json);
+    setcookie("login", $json, time()+3600, "/");
     echo "OK";
 } else {
     echo "Epic fail!";
--- coaching/www/ajax/getTransactions.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once '../functions.php';
-$account = val($_GET['account']);
-echo getTransactions($account);
-
-?>
