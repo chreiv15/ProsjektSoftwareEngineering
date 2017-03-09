6b2db9b1ca0513c6260362552a7dd3342ece24db
Fredrik Juel Hagen <fredrik.juel.hagen@gmail.com>
2017-03-09 10:57:39 +0100

    Fikset IFNULL-bug

--- ajax/getFormerSprint.php
+++ ajax/getFormerSprint.php
@@ -6,7 +6,7 @@ require_once 'val.php';
 $sprintId = val($_POST['sprintId']);
 $userId = val($_POST['userId']);
 
-$sql = "SELECT s.goal AS goalId, g.name as goalName, g.value AS goalValue, s.value AS sprintTarget, s.start AS sprintStart, s.end AS sprintEnd, g.date AS goalEnd, g.category AS goalCategory, c.icon AS goalIcon, c.name AS categoryName, SUM(t.value) AS sprintSpending FROM dnb_sprints AS s LEFT JOIN dnb_goals AS g ON s.goal = g.id LEFT JOIN dnb_accounts AS a ON a.owner = s.user LEFT JOIN dnb_transactions AS t ON t.account = a.id LEFT JOIN dnb_categories AS c ON c.id = g.category WHERE t.accounting_date > s.start AND t.accounting_date < s.end AND a.type = 1 AND s.user = $userId AND s.id = $sprintId;";
+$sql = "SELECT s.goal AS goalId, g.name as goalName, g.value AS goalValue, s.value AS sprintTarget, s.start AS sprintStart, s.end AS sprintEnd, g.date AS goalEnd, g.category AS goalCategory, c.icon AS goalIcon, c.name AS categoryName, ifnull(SUM(t.value), 0) AS sprintSpending FROM dnb_sprints AS s LEFT JOIN dnb_goals AS g ON s.goal = g.id LEFT JOIN dnb_accounts AS a ON a.owner = s.user LEFT JOIN dnb_transactions AS t ON t.account = a.id LEFT JOIN dnb_categories AS c ON c.id = g.category WHERE t.accounting_date > s.start AND t.accounting_date < s.end AND a.type = 1 AND s.user = $userId AND s.id = $sprintId;";
 
 $response = @mysqli_query($dbc, $sql);
 
