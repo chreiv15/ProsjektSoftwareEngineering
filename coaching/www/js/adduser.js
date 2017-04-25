1447d447b2a70987410868d0f7fa673ccd65634d
Fredrik Juel Hagen <fredrik.juel.hagen@gmail.com>
2017-04-25 14:07:25 +0200

    Nytt felt på ny bruker-side

--- coaching/www/js/adduser.js
+++ coaching/www/js/adduser.js
@@ -1,7 +1,6 @@
 function addUser() {
     
     $.post("http://fredrikhagen.no/westerdals/gruppe19/ajax/addUser.php", {
-        function: 'function', 
         fname: $("#f-name").val(),
         lname: $("#l-name").val(),
         email: $("#email").val()
