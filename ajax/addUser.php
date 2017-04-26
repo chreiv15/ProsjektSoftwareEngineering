e7925b3ca1b1c8f71afad0f0bf69ed519245d955
Fredrik Juel Hagen <fredrik.juel.hagen@gmail.com>
2017-04-26 12:18:14 +0200

    Lagt inn funksjon for å legge inn snittforbruk når man oppretter bruker.

--- ajax/addUser.php
+++ ajax/addUser.php
@@ -1,16 +1,16 @@
 <?php header('Access-Control-Allow-Origin: *');
-
-    if (isset($_POST["function"])) {
-        
+      
         require_once 'dbc.php';
         require_once 'val.php';
 
         $firstname = val($_POST['fname']);
         $lastname = val($_POST['lname']);
         $email = val($_POST['email']);
+        $spending = val($_POST['spending']);
         
-        $sql = "INSERT INTO dnb_users (firstname, lastname, email) VALUES('$firstname', '$lastname', '$email');";
+        $sql = "INSERT INTO dnb_users (firstname, lastname, email, `before`) VALUES('$firstname', '$lastname', '$email', '$spending');";
         $sql .= "SELECT LAST_INSERT_ID();";
+        //echo $sql;
 
         if(mysqli_multi_query($dbc,$sql)){
             do{
@@ -30,6 +30,5 @@
         }
 
         mysqli_close($dbc);
-    }
 
 ?>
--- coaching/www/js/adduser.js
+++ coaching/www/js/adduser.js
@@ -3,6 +3,7 @@ function addUser() {
     $.post("http://fredrikhagen.no/westerdals/gruppe19/ajax/addUser.php", {
         fname: $("#f-name").val(),
         lname: $("#l-name").val(),
+        spending: $("#spending").val(),
         email: $("#email").val()
     }, function(data) {
         console.log(data);
